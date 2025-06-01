aeb27e199a754cb5a03e7e6d5c6008c888816a26
esayas82 <esayasretta1@gmail.com>
2025-06-01 18:24:01 -0400

    Building payment page UI

--- src/Components/Pages/Payment/Payment.jsx
+++ src/Components/Pages/Payment/Payment.jsx
@@ -1,13 +1,90 @@
-import React from 'react'
-import classes from './Payment.module.css'
-import LayOut from '../../LayOut/LayOut'
+import React, { useContext, useState } from "react";
+import classes from "./Payment.module.css";
+import LayOut from "../../LayOut/LayOut";
+import { DataContext } from "../../DataProvider/DataProvider";
+import ProductCard from "../../Product/ProductCard";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from "../../CurrencyFormat/CurrencyFormat";
+
 
 function Payment() {
+  const [{ user, basket }, dispach] = useContext(DataContext);
+  console.log(user);
+  const totalItems = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
+
+  const total = basket?.reduce((amount, item) => {
+    return  item.price * item.amount + amount
+  }, 0);
+  const [cardError, setCardError] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleChange = (e) => {
+    console.log(e)
+e.error?.message? setCardError(e.error.message):setCardError(null)
+
+  }
   return (
     <LayOut>
-      <div>Payment</div>
+      {/* {Header} */}
+      <div className={classes.payment_header}>
+        Checkout ({totalItems}) items
+      </div>
+      {/*payment method*/}
+      <section className={classes.payment}>
+        {/* {address} */}
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            {" "}
+            <div>{user?.email || "Guest"}</div>
+            <div>123 Main St</div>
+            <div>chicago,IL</div>
+          </div>
+        </div>
+        <hr />
+        {/* {product} */}
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+            {basket?.map((item, index) => (
+              <ProductCard
+                key={index}
+                product={item}
+                flex={true}
+                renderDesc={false}
+                renderAdd={false}
+              />
+            ))}
+          </div>
+        </div>
+        <hr />
+        {/* {card form} */}
+        <div className={classes.flex}>
+          <h3>payment method</h3>
+          <div className={classes.payment_card_container}>
+            <div className={classes.payment_details}>
+              <form action="">
+                {/* {card error} */}
+                {cardError && <small style={{ color: "red" }}>{cardError}</small>}
+                {/* {card element} */}
+                <CardElement onChange={handleChange} />
+
+                {/*price*/}
+                <div className={classes.payment_price}>
+                  <div>
+                    <span style={{display:"flex",gap:"10px"}}><p>Total Order |</p><CurrencyFormat amount={total} /></span>
+                  </div>
+                  <button>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </LayOut>
-  )
+  );
 }
 
-export default Payment
+export default Payment;
