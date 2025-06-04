1901cee3075a6bc3a8c40ad5d1cc7346637d1db3
esayas82 <esayasretta1@gmail.com>
2025-06-04 09:25:06 -0400

    update

--- src/Components/Carousel/CarouselEffect.jsx
+++ src/Components/Carousel/CarouselEffect.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import { img } from './Img/data';
+import { img } from './img/data';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import classes from './CarouselEffect.module.css';
 
